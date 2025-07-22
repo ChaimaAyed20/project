c3574cb353af8d8f77d37dc39dd7e9eb08434f57
amelhassine <achouramel071@gmailcom>
2025-07-22 11:18:05 +0100

    add all files

--- database/migrations/2025_07_06_214416_create_albums_categories_table.php
+++ database/migrations/2025_07_06_214416_create_albums_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->boolean('photo')->default(false);
             $table->boolean('video')->default(false);
+            $table->timestamps();
         });
         
     }
